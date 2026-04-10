027bfd4fd643797a7cdd3e9effc997a6feef7fe8
yutaeho1 <yut02@naver.com>
2026-04-10 11:51:15 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,17 @@
+    #include <stdio.h>
+
+    int main() {
+        char *names[5] = {"kim", "lee", "park", "choi", "jung"};
+
+        for (int i=0; i<5; i++) {
+            for (int j=0; ; j++) {
+                char c = names[i][j];
+                if (c== '\0') 
+                    break;
+                printf("%c", c);
+            }
+            printf("\n");
+        }
+
+        return 0;
+    }
