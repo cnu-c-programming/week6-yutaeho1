d1cb6268678c5844f430d93a60c530c718cb4bf5
yutaeho1 <yut02@naver.com>
2026-04-12 19:12:14 +0900

    Update 3-4.c

---
+++ code/3-4.c
@@ -0,0 +1,13 @@
+    #include <stdio.h>
+
+    int main() {
+        int arr0[3] = {1, 2, 3};
+        int arr1[3] = {4, 5, 6};
+        int (*p[2])[3] = {&arr0, &arr1};
+
+        int x, y;
+        scanf("%d %d", &x, &y);
+
+        printf("%d\n", *(*(p+x)+y));
+        return 0;
+    }
